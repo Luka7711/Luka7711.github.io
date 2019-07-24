4c7ad6e53fad24c32d42c0a8fc4bbbcbdd0ca65b
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-07-24 13:08:22 -0500

    portfolio

---
+++ js/app.js
@@ -0,0 +1,45 @@
+let a = $(".project_image1");
+a.on("mouseover", function() {
+	$(".description1").css({"visibility":"visible", "opacity":"1", 
+		background: "linear-gradient(to-right, rgba(0, 0, 255, 0.3), rgba(10, 60, 180, 0.3))"})
+}).mouseout(function(){
+ 	$(".description1").css({"visibility":"hidden", "opacity":"0"})
+ });
+
+$(".project_image2").on("mouseover", function() {
+	$(".description2").css({"visibility":"visible", "opacity":"1", "color":"black"})
+}).mouseout(function(){
+ 	$(".description2").css({"visibility":"hidden", "opacity":"0"})
+ })
+
+
+$(".project_image3").on("mouseover", function() {
+	$(".description3").css({"visibility":"visible", "opacity":"1"})
+}).mouseout(function(){
+ 	$(".description3").css({"visibility":"hidden", "opacity":"0"})
+ })
+
+$(".project_image4").on("mouseover", function() {
+	$(".description4").css({"visibility":"visible", "opacity":"1"})
+}).mouseout(function(){
+ 	$(".description4").css({"visibility":"hidden", "opacity":"0"})
+ })
+
+let insta = ["url(./images/brooklyn.png)", "url(./images/chinatown.png)", "url(./images/hometown.jpg)", 
+"url(./images/indiana.png)", "url(./images/kansas.png)", "url(./images/saltlake.png)", 
+"url(./images/street.png)", "url(./images/beaty.png)"];;
+
+
+
+for(let i=0; i<insta.length; i++){
+	$divs = $('.insta_img').get(i);
+	$($divs).css("background-image", insta[i])
+}
+
+
+
+
+
+
+
+
